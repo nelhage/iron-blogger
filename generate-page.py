0d15f2365b270c0e5c101c65e77371b4c7ada00c
Nelson Elhage <nelhage@mit.edu>
2009-12-28 17:13:19 -0500

    Initial script to generate website.

---
+++ generate-page.py
@@ -0,0 +1,24 @@
+import yaml
+from mako.template import Template
+
+with open('bloggers.yml') as f:
+    users = yaml.safe_load(f.read())
+
+class User(object):
+    pass
+
+all = []
+for (un, rec) in users.items():
+    u = User()
+    u.username = un
+    u.links = rec['links']
+    u.start = rec['start']
+    all.append(u)
+
+def user_key(u):
+    return (u.start, u.username)
+
+all.sort(key=user_key)
+
+tmpl = Template(filename='iron-blogger.tmpl', output_encoding='utf-8')
+print tmpl.render(users=all)
