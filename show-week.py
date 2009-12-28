364ecc7623bd4a3940ca1876d639777471df0de4
Nelson Elhage <nelhage@mit.edu>
2009-12-28 18:41:20 -0500

    show-week: use a mako template
    Add a template for sending email, as well.

--- show-week.py
+++ show-week.py
@@ -3,6 +3,7 @@ from dateutil.parser import parse
 import datetime
 import dateutil.tz as tz
 import sys
+from mako.template import Template
 
 START = datetime.datetime(2009, 12, 21, 6)
 
@@ -16,13 +17,21 @@ week = (parse(sys.argv[1], default=START) - START).days / 7
 week_start = START + (week * datetime.timedelta(7))
 week_end   = START + ((week + 1) * datetime.timedelta(7))
 
+good = []
+lame = []
+skip = []
+
 
 for (name, weeks) in report.items():
     user_start = parse(users[name]['start'], default=START)
     if user_start > week_start:
-        print "SKIP %s" % (name,)
+        skip.append(name)
         continue
     elif len(weeks) <= week or not weeks[week]:
-        print "LAME %s" % (name,)
+        lame.append(name)
     else:
-        print "OK %s" % (name,)
+        good.append(name)
+
+print Template(filename=sys.argv[2], output_encoding='utf-8').render(
+    week=week, week_start=week_start,week_end=week_end,
+    good=good, lame=lame, skip=skip, users=users, report=report)
