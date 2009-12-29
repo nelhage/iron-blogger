c631489c69e4f820bd2469d44ab2b17295f6a341
Nelson Elhage <nelhage@mit.edu>
2009-12-29 14:05:36 -0500

    Generalize show-week slightly.

--- show-week.py
+++ show-week.py
@@ -4,9 +4,20 @@ from dateutil.parser import parse
 import datetime
 import dateutil.tz as tz
 import sys
+import os
 from mako.template import Template
 
+if len(sys.argv) < 2:
+    print >>sys.stderr, "Usage: %s TEMPLATE [WEEK]" 
+    sys.exit(1)
+
+template = sys.argv[1]
 START = datetime.datetime(2009, 12, 21, 6)
+
+if len(sys.argv) == 3:
+    week = parse(sys.argv[2], default=START)
+else:
+    week = START
 
 with open('out/report.yml') as r:
     report = yaml.safe_load(r)
@@ -14,25 +25,41 @@ with open('out/report.yml') as r:
 with open('bloggers.yml') as f:
     users = yaml.safe_load(f)
 
-week = (parse(sys.argv[1], default=START) - START).days / 7
+week = (week - START).days / 7
 week_start = START + (week * datetime.timedelta(7))
 week_end   = START + ((week + 1) * datetime.timedelta(7))
 
 good = []
 lame = []
 skip = []
+userlist = []
 
+class User(object):
+    pass
 
-for (name, weeks) in report.items():
-    user_start = parse(users[name]['start'], default=START)
+for (un, rec) in users.items():
+    u = User()
+    u.username = un
+    u.links = rec['links']
+    u.start = rec['start']
+    u.weeks = report.get(un, [])
+
+    userlist.append(u)
+
+def user_key(u):
+    return (u.start, u.username)
+
+userlist.sort(key=user_key)
+
+for u in userlist:
+    user_start = parse(u.start, default=START)
     if user_start > week_start:
-        skip.append(name)
-        continue
-    elif len(weeks) <= week or not weeks[week]:
-        lame.append(name)
+        skip.append(u)
+    elif len(u.weeks) <= week or not u.weeks[week]:
+        lame.append(u)
     else:
-        good.append(name)
+        good.append(u)
 
-print Template(filename=sys.argv[2], output_encoding='utf-8').render(
+print Template(filename=template, output_encoding='utf-8').render(
     week=week, week_start=week_start,week_end=week_end,
-    good=good, lame=lame, skip=skip, users=users, report=report)
+    good=good, lame=lame, skip=skip, userlist=userlist)
